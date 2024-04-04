71d5936e9c1a28c3f5d99001d6e0a5528ad7206a
cms7739 <curt.strab@iff.com>
2024-04-04 12:26:10 -0400

    update script variable names

--- setup/database/create_init.sql.sh
+++ setup/database/create_init.sql.sh
@@ -2,22 +2,22 @@
 
 # Check if three arguments were provided
 if [ $# -ne 4 ]; then
-    echo "Four arguments required: schema file, data type mapping file, database type, and output file"
+    echo "Four arguments required: schema file, database type mapping file, database type, and output file"
     exit 1
 fi
 
 # Read the schema.json file and data type mapping file
-schema=$(cat $1)
-data_type_mapping=$(cat $2)
+schema_file=$(cat $1)
+database_type_mapping=$(cat $2)
 database_type=$3
 output_file=$4
 
 # Use jq to parse the JSON and generate the SQL commands
 echo -e "-- Create the schema" > $output_file
-echo -e "CREATE SCHEMA IF NOT EXISTS $(echo $schema | jq -r .schema_name);\n" >> $output_file
+echo -e "CREATE SCHEMA IF NOT EXISTS $(echo $schema_file | jq -r .schema_name);\n" >> $output_file
 
 # Loop over the tables
-for table in $(echo $schema | jq -r '.tables[] | @base64'); do
+for table in $(echo $schema_file | jq -r '.tables[] | @base64'); do
     # Decode the table JSON
     table=$(echo $table | base64 --decode)
 
@@ -26,7 +26,7 @@ for table in $(echo $schema | jq -r '.tables[] | @base64'); do
 
     # Start the CREATE TABLE command
     echo -e "-- Create $table_name" >> $output_file
-    echo -e -n "CREATE TABLE IF NOT EXISTS $(echo $schema | jq -r .schema_name).$table_name (\n" >> $output_file
+    echo -e -n "CREATE TABLE IF NOT EXISTS $(echo $schema_file | jq -r .schema_name).$table_name (\n" >> $output_file
 
     # Loop over the columns
     first_column=true
@@ -39,7 +39,7 @@ for table in $(echo $schema | jq -r '.tables[] | @base64'); do
         data_type=$(echo $column | jq -r .data_type)
 
         # Map the data type to a database-specific data type
-        data_type=$(echo $data_type_mapping | jq -r ".$database_type.$data_type")
+        data_type=$(echo $database_type_mapping | jq -r ".$database_type.$data_type")
 
         # Add a comma if this is not the first column
         if [ "$first_column" != true ]; then
