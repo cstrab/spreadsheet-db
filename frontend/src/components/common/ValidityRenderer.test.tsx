8db353367eb2eccd64678001154ea700e9ebaed0
cstrab <cstrab@outlook.com>
2024-05-22 11:03:39 -0400

    frontend unit tests

--- frontend/src/components/common/ValidityRenderer.test.tsx
+++ frontend/src/components/common/ValidityRenderer.test.tsx
@@ -2,27 +2,27 @@ import { render } from '@testing-library/react';
 import ValidityRenderer from './ValidityRenderer';
 
 describe('ValidityRenderer', () => {
-  it('should render "Valid" when isValid is true', () => {
-    const props = {
-      ...jest.fn()(),
-      data: {
-        isValid: true,
-      },
-    };
+    it('should render "Valid" when isValid is true', () => {
+      const props = {
+        ...jest.fn()(),
+        data: {
+          isValid: true,
+        },
+      };
 
-    const { getByText } = render(<ValidityRenderer {...props} />);
-    expect(getByText('Valid')).toBeInTheDocument();
-  });
+      const { getByText } = render(<ValidityRenderer {...props} />);
+      expect(getByText('Valid')).toBeInTheDocument();
+    });
 
-  it('should render "Invalid" when isValid is false', () => {
-    const props = {
-      ...jest.fn()(),
-      data: {
-        isValid: false,
-      },
-    };
+    it('should render "Invalid" when isValid is false', () => {
+      const props = {
+        ...jest.fn()(),
+        data: {
+          isValid: false,
+        },
+      };
 
-    const { getByText } = render(<ValidityRenderer {...props} />);
-    expect(getByText('Invalid')).toBeInTheDocument();
-  });
+      const { getByText } = render(<ValidityRenderer {...props} />);
+      expect(getByText('Invalid')).toBeInTheDocument();
+    });
 });
